fcc3f1e6e4ed24561fc8070a77154ee976e63fde
StanislavKharkiv <shtas89@gmail.com>
2020-01-16 18:01:21 +0200

    feat: add text animation, buttons animation, web animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,38 @@
-const range = document.documentElement.clientWidth / 2;
-    var myPath = anime.path('svg path');
+const range = document.documentElement.clientWidth;
+
     anime({
       targets: '.menu__item',
       translateX: range,
       direction: 'alternate',
       loop: false,
       delay: function (el, i, l) {
-        return i * 300;
+        return i * 200;
       },
-      duration: 2000,
+      duration: 1300,
       endDelay: function (el, i, l) {
         return (l - i) * 500;
       },
       complete: function(anim) {
-        console.log('complete')
+        document.getElementById('main-bg-svg').style.display = 'block';
         anime({
           targets: '.svg__path',
           strokeDashoffset: [anime.setDashoffset, 0],
           easing: 'easeInOutSine',
-          duration: 3000,
-          delay: function (el, i) { return i * 2000 },
+          duration: 4000,
+          delay: function (el, i) { return i * 800 },
           direction: 'alternate',
-          loop: true
+          loop: false
         });
       }
     });
+    anime({
+      targets: '.el',
+      scale: [
+        {value: .1, easing: 'easeOutSine', duration: 1000},
+        {value: 1, easing: 'easeInOutQuad', duration: 500}
+      ],
+      delay: anime.stagger(100, {grid: [14, 5], from: 'first'}),
+    });
+    
 
    
