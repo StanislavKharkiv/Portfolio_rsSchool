a00ed9f3fb03e6d2233e2008e8561285f4ef1def
StanislavKharkiv <shtas89@gmail.com>
2020-01-29 14:17:45 +0200

    add pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,20 @@ const state = {
 
 const mainBlock = document.getElementById('main');
 const menuBlock = document.getElementById('menu');
-const documentWidth = document.documentElement.clientWidth;
+let documentWidth = document.documentElement.clientWidth;
 createTextScreen();
 menuAnimation(documentWidth, bgAnimation);
-
+  // events
+window.addEventListener('resize', function() {
+  documentWidth = document.documentElement.clientWidth;
+  menuAnimation(documentWidth);
+})
 menuBlock.addEventListener('click', function(e) {
   if (e.target.closest('.menu__item')) {
     hideMenu();
+    if (e.target.closest('.btn-contacts')) addSectionContacts();
+    if (e.target.closest('.btn-about')) addSectionAbout();
+    if (e.target.closest('.btn-works')) addSectionWorks();
   }
   if (e.target.closest('.menu__btn')) {
     showMenu();
@@ -59,24 +66,60 @@ function showMenu() {
 }
 
 // Contacts
-function addSectionContact() {    
-  const contact = document.createElement('section');
-  contact.className = 'contact';
-  contact.innerHTML = `
-  <div class="contact__phone"><a href="tel:+380982231377">+380982231377</a></div>
-  <div class="contact__email"><a href="mailto:shtas89@gmail.com">shtas89@gmail.com</a></div>
-  <div class="contact__social">
-    <div class="telegram">telegram</div>
-    <div class="viber">viber</div>
-    <div class="skype">skype</div>
+function addSectionContacts() {    
+  const contacts = document.createElement('section');
+  contacts.className = 'contacts page';
+  contacts.innerHTML = `
+  <div class="contacts-wrapper">
+    <div class="contacts__phone"><a href="tel:+380982231377">+380982231377</a></div>
+    <div class="contacts__email"><a href="mailto:shtas89@gmail.com">shtas89@gmail.com</a></div>
+    <div class="contacts__social">
+      <div class="telegram">
+        <a href="https://teleg.run/StanislavUaKh">Telegram</a>
+      </div>
+      <div class="viber">
+        <a href="https://msng.link/o/?380982231377=vi">Viber</a>
+      </div>
+      <div class="skype">
+        <a href="https://msng.link/o/?shtas89=sk">Skype</a>
+      </div>
+    </div>
   </div>
   `;
+
+  addSectionToDom(contacts);
 }
 // About
-
+function addSectionAbout() {
+  const about = document.createElement('section');
+  about.className = 'about page';
+  about.innerHTML = `
+    <div class="about-wrapper">
+      <h2 class="about__h2">Станислав Иосифов</h2>
+      <p class="about__profession">Frontend developer</p>
+    </div>
+  `
+  addSectionToDom(about);
+}
 
 // Works
+function addSectionWorks() {
+  const works = document.createElement('section');
+  works.className = 'works page';
+  works.innerHTML = `
+    <div class="works-wrapper">
+      <h2 class="works__h2">галлерея</h2>
+      <div class="works__wrap">... здесь будут мои работы ...</div>
+    </div>
+  `
+  addSectionToDom(works);
+}
 
+function addSectionToDom(node) {
+  const page = document.querySelector('.page');
+  if (!page) mainBlock.appendChild(node);
+  mainBlock.replaceChild(node, page)
+}
 /* animation */
 
 function menuAnimation(range = documentWidth, cb = null) {
